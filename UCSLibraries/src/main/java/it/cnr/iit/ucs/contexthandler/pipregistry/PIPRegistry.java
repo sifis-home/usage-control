35be3966df3e6ef9740d956621edd2d112a1f214
marco-rasori <marco.rasori@gmail.com>
2022-06-13 12:22:14 +0200

    Added method to subscribe only to the PIPs managing ongoing attributes. For non-environment attributes, the method uses the original request to retrieve subject-related info.

--- UCSLibraries/src/main/java/it/cnr/iit/ucs/contexthandler/pipregistry/PIPRegistry.java
+++ UCSLibraries/src/main/java/it/cnr/iit/ucs/contexthandler/pipregistry/PIPRegistry.java
@@ -9,6 +9,7 @@ import com.google.common.base.Throwables;
 
 import it.cnr.iit.ucs.pip.PIPCHInterface;
 import it.cnr.iit.xacml.Attribute;
+import it.cnr.iit.xacml.Category;
 import oasis.names.tc.xacml.core.schema.wd_17.RequestType;
 
 public class PIPRegistry implements PIPRegistryInterface {
@@ -94,4 +95,45 @@ public class PIPRegistry implements PIPRegistryInterface {
 		return getByAttribute(attribute).isPresent();
 	}
 
+
+	/**
+	 * Given a list of attributes, invokes the PIPs' subscribe method
+	 * for each attribute, if that attribute is owned by a PIP.
+	 * If an attribute has category different from ENVIRONMENT, e.g.,
+	 * is a SUBJECT-related attribute, use the request to retrieve
+	 * the value of the SUBJECT attribute and set it as
+	 * 'additionalInformation' of the attribute. This is needed by
+	 * the PIP for subscribing to the right attribute value.
+	 * For example, a PIPReader responsible for the 'role' attribute
+	 * (a SUBJECT-related attribute) needs to know the 'subject-id'
+	 * to monitor.
+	 *
+	 * @param request the request
+	 * @param attributes the list of attributes to subscribe to. It
+	 *                   can contain attributes not managed by any PIP.
+	 */
+	@Override
+	public void subscribe(RequestType request, List<Attribute> attributes) {
+		for (PIPCHInterface pip : pipList) {
+			int attributesNumber = pip.getAttributes().size();
+			for (int i = 0; i < attributesNumber; i++) {
+				Attribute pipAttribute = pip.getAttributes().get(i);
+				Category category = pipAttribute.getCategory();
+				for (Attribute a : attributes) {
+					if (pipAttribute.getAttributeId().equals(a.getAttributeId())) {
+						try {
+							if (!category.equals(Category.ENVIRONMENT)) {
+								String filter = request.getAttributeValue(category);
+								a.setAdditionalInformations(filter);
+							}
+							pip.subscribe(a);
+						} catch (Exception e) {
+							log.severe("Error subscribe : " + e.getMessage());
+							Throwables.throwIfUnchecked(new RuntimeException("Error subscribe : " + e.getMessage()));
+						}
+					}
+				}
+			}
+		}
+	}
 }
--- UCSLibraries/src/main/java/it/cnr/iit/ucs/contexthandler/pipregistry/PIPRegistryInterface.java
+++ UCSLibraries/src/main/java/it/cnr/iit/ucs/contexthandler/pipregistry/PIPRegistryInterface.java
@@ -33,4 +33,6 @@ public interface PIPRegistryInterface {
 
     public boolean hasAttribute( Attribute attribute );
 
+    public void subscribe(RequestType request, List<Attribute> attributes);
+
 }
