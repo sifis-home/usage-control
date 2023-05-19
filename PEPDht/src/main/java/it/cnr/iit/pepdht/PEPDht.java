ee7c867d992f3d446b3a590ce494b29cd5562091
marco-rasori <marco.rasori@gmail.com>
2023-05-19 17:41:25 +0200

    Changes
    - add the structures to keep track of accesses and unanswered requests
    - add endAccess request
    - add code documentation
    - tidy up methods

--- PEPDht/src/main/java/it/cnr/iit/pepdht/PEPDht.java
+++ PEPDht/src/main/java/it/cnr/iit/pepdht/PEPDht.java
@@ -1,15 +1,17 @@
 package it.cnr.iit.pepdht;
 
 import com.google.gson.JsonSyntaxException;
+import it.cnr.iit.pepdht.track.AccessTracker;
 import it.cnr.iit.ucs.message.Message;
 import it.cnr.iit.ucs.message.reevaluation.ReevaluationResponseMessage;
 import it.cnr.iit.ucs.pep.PEPInterface;
-
 import it.cnr.iit.ucs.properties.components.PepProperties;
 import it.cnr.iit.utility.dht.DHTClient;
 import it.cnr.iit.utility.dht.jsondht.JsonIn;
 import it.cnr.iit.utility.dht.jsondht.JsonOut;
 import it.cnr.iit.utility.dht.jsondht.MessageContent;
+import it.cnr.iit.utility.dht.jsondht.endaccess.EndAccessRequest;
+import it.cnr.iit.utility.dht.jsondht.endaccess.EndAccessResponse;
 import it.cnr.iit.utility.dht.jsondht.startaccess.StartAccessRequest;
 import it.cnr.iit.utility.dht.jsondht.startaccess.StartAccessResponse;
 import it.cnr.iit.utility.dht.jsondht.tryaccess.TryAccessRequest;
@@ -25,7 +27,13 @@ import static it.cnr.iit.utility.dht.DHTUtils.*;
 
 public class PEPDht implements PEPInterface {
 
-    private static ConcurrentMap<String, JsonOut> unansweredMap = new ConcurrentHashMap<>();
+    // map containing the requests sent and for which a response has not been received yet.
+    // The messageId is the key and the json object (the request) is the value
+    private static final ConcurrentMap<String, JsonOut> unansweredMap = new ConcurrentHashMap<>();
+
+    // object that keeps track of the status of requests and answers.
+    // Useful to retrieve the sessionId from a messageId
+    private static final AccessTracker accessTracker = new AccessTracker();
     private static DHTClient dhtClientEndPoint;
     private static final String COMMAND_TYPE = "pep-command";
     private static final String SUB_COMMAND_TYPE = "ucs-command";
@@ -66,15 +74,7 @@ public class PEPDht implements PEPInterface {
     private static void tryAccess() {
         // build the json object
         JsonOut jsonOut = buildTryAccessMessage();
-
-        // serialize it to a json string
-        String msg = serializeOutgoingJson(jsonOut);
-
-        // send the request
-        if (dhtClientEndPoint.sendMessage(msg)) {
-            String messageId = jsonOut.getRequestPubMessage().getValue().getCommand().getValue().getMessage().getMessage_id();
-            unansweredMap.put(messageId, jsonOut);
-        }
+        serializeAndSend(jsonOut);
     }
 
 
@@ -86,25 +86,184 @@ public class PEPDht implements PEPInterface {
     private static JsonOut buildTryAccessMessage() {
         TryAccessRequest message =
                 new TryAccessRequest("random123-msg_id", "request", "policy");
-                //new TryAccessRequest(String.valueOf(UUID.randomUUID()), "request", "policy");
+        //new TryAccessRequest(String.valueOf(UUID.randomUUID()), "request", "policy");
 
         return buildOutgoingJsonObject(message, PEP_ID, PUB_TOPIC_NAME, PUB_TOPIC_UUID, COMMAND_TYPE);
+    }
+
+    /**
+     * Publish a startAccess request on the DHT
+     *
+     * @param sessionId the session identifier
+     */
+    private static void startAccess(String sessionId) {
+        // build the json object
+        JsonOut jsonOut = buildStartAccessMessage(sessionId);
+        serializeAndSend(jsonOut);
     }
 
 
     /**
      * Build a startAccess request as a Json to send to the DHT
      *
-     * @param session_id the session identifier, as extracted from
-     *                   a tryAccess response
+     * @param sessionId the session identifier, as extracted from
+     *                  a tryAccess response
      * @return the Json to send to the DHT
      */
-    private static JsonOut buildStartAccessMessage(String session_id) {
+    private static JsonOut buildStartAccessMessage(String sessionId) {
         StartAccessRequest message =
-                new StartAccessRequest("random456-msg_id", session_id);
-                //new StartAccessRequest(String.valueOf(UUID.randomUUID()), session_id);
+                new StartAccessRequest("random456-msg_id", sessionId);
+        //new StartAccessRequest(String.valueOf(UUID.randomUUID()), session_id);
 
         return buildOutgoingJsonObject(message, PEP_ID, PUB_TOPIC_NAME, PUB_TOPIC_UUID, COMMAND_TYPE);
+    }
+
+
+    /**
+     * Publish an endAccess request on the DHT
+     *
+     * @param sessionId the session identifier
+     */
+    private static void endAccess(String sessionId) {
+        // build the json object
+        JsonOut jsonOut = buildEndAccessMessage(sessionId);
+        serializeAndSend(jsonOut);
+    }
+
+
+    /**
+     * Build an endAccess request as a Json to send to the DHT
+     *
+     * @param sessionId the session identifier
+     * @return the Json to send to the DHT
+     */
+    private static JsonOut buildEndAccessMessage(String sessionId) {
+        EndAccessRequest message =
+                new EndAccessRequest("random789-msg_id", sessionId);
+        //new EndAccessRequest(String.valueOf(UUID.randomUUID()), session_id);
+
+        return buildOutgoingJsonObject(message, PEP_ID, PUB_TOPIC_NAME, PUB_TOPIC_UUID, COMMAND_TYPE);
+    }
+
+
+    /**
+     * Serialize the JsonOut object passed as argument and publish the
+     * Json string on the DHT. If this succeeds, update the structures
+     * to keep track of the access and add this request to the unanswered.
+     *
+     * @param jsonOut the object to serialize and send
+     */
+    private static void serializeAndSend(JsonOut jsonOut) {
+        // serialize the object to a json string
+        String msg = serializeOutgoingJson(jsonOut);
+
+        // send the request
+        if (dhtClientEndPoint.sendMessage(msg)) {
+            MessageContent message = jsonOut.getRequestPubMessage().getValue().getCommand().getValue().getMessage();
+            unansweredMap.put(message.getMessage_id(), jsonOut);
+            accessTracker.add(message);
+        }
+    }
+
+
+    /**
+     * Check the evaluation. In case of Permit, (TODO: grants the access and)
+     * publishes a startAccess request on the DHT.
+     * In any other case, denies the access.
+     *
+     * @param message the tryAccess response received from the DHT
+     */
+    private static void handleTryAccessResponse(TryAccessResponse message) {
+        // check the evaluation
+        if (!message.getEvaluation().equalsIgnoreCase("Permit")) {
+            System.out.println("Access denied. TryAccess evaluated to: " + message.getEvaluation());
+            return;
+        }
+        // if Permit:
+        //  - grant access
+        //  - send a start access request
+        startAccess(message.getSession_id());
+    }
+
+
+    /**
+     * Check the evaluation. In case is not Permit, denies the access.
+     *
+     * @param message the startAccess response received from the DHT
+     */
+    private static void handleStartAccessResponse(StartAccessResponse message) {
+        // check the evaluation
+        if (!message.getEvaluation().equalsIgnoreCase("Permit")) {
+            System.out.println("Access denied. StartAccess evaluated to: " + message.getEvaluation());
+            // terminate access
+            return;
+        }
+        // if Permit, keep granting access
+        System.out.println("Ongoing access is granted");
+
+        // simulate usage for some time
+        try {
+            Thread.sleep(3000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
+
+        // access terminates naturally
+        // send an end access request
+        String sessionId = accessTracker.getSessionId(message.getMessage_id());
+        endAccess(sessionId);
+    }
+
+
+    /**
+     * Print the evaluation. Actually, the evaluation doesn't matter for
+     * the access, since it is already terminated.
+     *
+     * @param message the startAccess response received from the DHT
+     */
+    private static void handleEndAccessResponse(EndAccessResponse message) {
+        // print the evaluation
+        System.out.println("EndAccess evaluated to: " + message.getEvaluation());
+    }
+
+
+    /**
+     * This method is executed when a message with a valid topic
+     * is received from the DHT.
+     * It handles the message according to its type
+     */
+    private static void processMessage(MessageContent message) {
+
+        // check that we are waiting a response for this message_id
+        if (!unansweredMap.containsKey(message.getMessage_id())) {
+            System.out.println("The received message_id is not associated with " +
+                    "any unanswered request that we sent.");
+            return;
+        } else {
+            unansweredMap.remove(message.getMessage_id());
+        }
+
+        accessTracker.add(message);
+
+        if (message instanceof TryAccessResponse) {
+            // handle try access response
+            System.out.println("handle try access response");
+            handleTryAccessResponse((TryAccessResponse) message);
+        } else if (message instanceof StartAccessResponse) {
+            // handle start access response
+            System.out.println("handle start access response");
+            handleStartAccessResponse((StartAccessResponse) message);
+        } else if (message instanceof EndAccessResponse) {
+            // handle end access response
+            handleEndAccessResponse((EndAccessResponse) message);
+//        } else if (message instanceof RevokeAccessResponse) {
+//            // handle start access response
+//            return;
+        } else {
+            // class not recognized. Handle case
+            // this should not happen since the deserialization would already have thrown an exception
+            System.err.println("class not recognized. It might be a ResponseMessage");
+        }
     }
 
 
@@ -125,105 +284,20 @@ public class PEPDht implements PEPInterface {
 
 
     /**
-     * Check the evaluation. In case of Permit, (TODO: grants the access and)
-     * publishes a startAccess request on the DHT.
-     * In any other case, denies the access.
+     * Message handler that implements the handleMessage method deserializes the received
      *
-     * @param message the tryAccess response received from the DHT
+     * @return the message handler
      */
-    private static void handleTryAccessResponse(TryAccessResponse message) {
-        // check the evaluation
-        if (!message.getEvaluation().equalsIgnoreCase("Permit")) {
-            System.out.println("Access denied. TryAccess evaluated to: " + message.getEvaluation());
-            return;
-        }
-        // if Permit:
-        //  - grant access
-        //  - save the session_id
-        //  - send a start access request
-        String sessionId = message.getSession_id();
-        startAccess(sessionId);
-    }
-
-    private static void startAccess(String sessionId) {
-        // build the json object
-        JsonOut jsonOut = buildStartAccessMessage(sessionId);
-
-        // serialize it to a json string
-        String msg = serializeOutgoingJson(jsonOut);
-
-        // send the request
-        if (dhtClientEndPoint.sendMessage(msg)) {
-            String messageId = jsonOut.getRequestPubMessage().getValue().getCommand().getValue().getMessage().getMessage_id();
-            unansweredMap.put(messageId, jsonOut);
-        }
-    }
-
-    /**
-     * Check the evaluation. In case of Permit, grants the access.
-     * In any other case, denies the access.
-     *
-     * @param message the startAccess response received from the DHT
-     */
-    private static void handleStartAccessResponse(StartAccessResponse message) {
-        // check the evaluation
-        if (!message.getEvaluation().equalsIgnoreCase("Permit")) {
-            System.out.println("Access denied. StartAccess evaluated to: " + message.getEvaluation());
-            // terminate access
-            // send an end access request
-            return;
-        }
-        // if Permit, grant access
-        System.out.println("Access is granted");
-    }
-
-
-    /**
-     * This method is executed when a message with a valid topic
-     * is received from the DHT.
-     * It handles the message according to its type
-     */
-    private static void processMessage(MessageContent message) {
-
-        // check that we are waiting a response for this message_id
-        if (!unansweredMap.containsKey(message.getMessage_id())) {
-            System.out.println("The received message_id is not associated with" +
-                    "any unanswered request that we sent.");
-            return;
-        } else {
-            unansweredMap.remove(message.getMessage_id());
-        }
-
-        if (message instanceof TryAccessResponse) {
-            // handle try access response
-            System.out.println("handle try access response");
-            handleTryAccessResponse((TryAccessResponse) message);
-            return;
-        } else if (message instanceof StartAccessResponse) {
-            // handle start access response
-            System.out.println("handle start access response");
-            handleStartAccessResponse((StartAccessResponse) message);
-            return;
-//        } else if (message instanceof EndAccessResponse) {
-//            // handle start access response
-//            return;
-//        } else if (message instanceof RevokeAccessResponse) {
-//            // handle start access response
-//            return;
-        } else {
-            // class not recognized. Handle case
-            // this should not happen since the deserialization would already have thrown an exception
-            System.out.println("class not recognized. It might be a ResponseMessage");
-            return;
-        }
-    }
-
-
     private static DHTClient.MessageHandler setMessageHandler() {
         DHTClient.MessageHandler messageHandler = new DHTClient.MessageHandler() {
+            /**
+             * Deserialize the received message and check the topic matches
+             * the one the PEP is subscribed to. If so, process the response
+             * coming from the DHT
+             *
+             * @param message the message, a json string, coming from the DHT
+             */
             public void handleMessage(String message) {
-                //System.out.println("Received new message\n");
-
                 MessageContent msg;
                 try {
                     // deserialize json
@@ -239,15 +313,8 @@ public class PEPDht implements PEPInterface {
                     System.err.println("Error deserializing Json. " + e.getMessage());
                     return;
                 }
-
                 processMessage(msg);
-
-//                receiveResponse(message);
             }
-
-//            private void process() {
-//                System.out.println("sw");
-//            }
         };
         return messageHandler;
     }
