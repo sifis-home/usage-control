d09babd333b4d455c3207dde756b1d4cf5c69d6e
marco-rasori <marco.rasori@gmail.com>
2023-05-19 17:22:47 +0200

    Add classes to keep track of the sent requests and received responses

---
+++ PEPDht/src/main/java/it/cnr/iit/pepdht/track/AccessTracker.java
@@ -0,0 +1,107 @@
+package it.cnr.iit.pepdht.track;
+
+import it.cnr.iit.utility.dht.jsondht.MessageContent;
+import it.cnr.iit.utility.dht.jsondht.endaccess.EndAccessRequest;
+import it.cnr.iit.utility.dht.jsondht.endaccess.EndAccessResponse;
+import it.cnr.iit.utility.dht.jsondht.startaccess.StartAccessRequest;
+import it.cnr.iit.utility.dht.jsondht.startaccess.StartAccessResponse;
+import it.cnr.iit.utility.dht.jsondht.tryaccess.TryAccessRequest;
+import it.cnr.iit.utility.dht.jsondht.tryaccess.TryAccessResponse;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+public class AccessTracker {
+    private Map<String, List<String>> msgIdsPerSession = new HashMap<>();
+    private Map<String, MessageInfo> msgFlow = new HashMap<>();
+
+    public boolean add(MessageContent message) {
+        if (msgFlow.containsKey(message.getMessage_id())) {
+            return mergeMessages(message);
+        } else if (message instanceof TryAccessRequest) {
+            return addNewMessage(message);
+        } else if (message instanceof StartAccessRequest) {
+            addMessageId(((StartAccessRequest) message).getSession_id(), message.getMessage_id());
+            return addNewMessage(message);
+        } else if (message instanceof EndAccessRequest) {
+            addMessageId(((EndAccessRequest) message).getSession_id(), message.getMessage_id());
+            return addNewMessage(message);
+        } else {
+            throw new IllegalArgumentException("Invalid message");
+        }
+    }
+
+
+    private boolean mergeMessages(MessageContent message) {
+        MessageInfo messageInfo = msgFlow.get(message.getMessage_id());
+        if (message instanceof TryAccessResponse) {
+            addMessagePerSession((TryAccessResponse) message);
+            messageInfo.merge((TryAccessResponse) message);
+        } else if (message instanceof StartAccessResponse) {
+            messageInfo.merge((StartAccessResponse) message);
+        } else if (message instanceof EndAccessResponse) {
+            messageInfo.merge((EndAccessResponse) message);
+        }
+        return insert(message.getMessage_id(), messageInfo);
+    }
+
+
+    private boolean addNewMessage(MessageContent message) {
+        MessageInfo messageInfo = null;
+        if (message instanceof TryAccessRequest) {
+            messageInfo = MessageInfo.build((TryAccessRequest) message);
+        } else if (message instanceof StartAccessRequest) {
+            messageInfo = MessageInfo.build((StartAccessRequest) message);
+        } else if (message instanceof EndAccessRequest) {
+            messageInfo = MessageInfo.build((EndAccessRequest) message);
+        }
+        return insert(message.getMessage_id(), messageInfo);
+    }
+
+
+    private boolean insert(String messageId, MessageInfo messageInfo) {
+        if (messageId == null || messageInfo == null) {
+            System.err.println("Insertion in msgFlow aborted: null arguments");
+            return false;
+        }
+        msgFlow.put(messageId, messageInfo);
+        return true;
+    }
+
+
+    public List<String> getMessagesPerSession(String sessionId) {
+//        Reject.ifBlank( sessionId );
+        if (!msgIdsPerSession.containsKey(sessionId)) {
+            throw new IllegalArgumentException();
+        }
+        return msgIdsPerSession.get(sessionId);
+    }
+
+
+    private void addMessagePerSession(TryAccessResponse message) {
+        if (message.getEvaluation().equalsIgnoreCase("Permit")) {
+            msgIdsPerSession.put(message.getSession_id(), new ArrayList<>());
+            addMessageId(message.getSession_id(), message.getMessage_id());
+        }
+    }
+
+
+    private void addMessageId(String sessionId, String messageId) {
+        msgIdsPerSession.get(sessionId).add(messageId);
+    }
+
+    public String getSessionId(String messageId) {
+        if (!msgFlow.containsKey(messageId)) {
+            throw new IllegalArgumentException();
+        }
+        MessageInfo messageInfo = msgFlow.get(messageId);
+        return messageInfo.getSessionId();
+    }
+
+    public void clear() {
+        msgFlow.clear();
+        msgIdsPerSession.clear();
+    }
+}
---
+++ PEPDht/src/main/java/it/cnr/iit/pepdht/track/MessageInfo.java
@@ -0,0 +1,80 @@
+package it.cnr.iit.pepdht.track;
+
+import it.cnr.iit.utility.dht.jsondht.EvaluatedMessageContent;
+import it.cnr.iit.utility.dht.jsondht.endaccess.EndAccessRequest;
+import it.cnr.iit.utility.dht.jsondht.endaccess.EndAccessResponse;
+import it.cnr.iit.utility.dht.jsondht.startaccess.StartAccessRequest;
+import it.cnr.iit.utility.dht.jsondht.startaccess.StartAccessResponse;
+import it.cnr.iit.utility.dht.jsondht.tryaccess.TryAccessRequest;
+import it.cnr.iit.utility.dht.jsondht.tryaccess.TryAccessResponse;
+
+public class MessageInfo {
+
+    private STATUS status;
+    private String sessionId;
+
+    private static MessageInfo build(STATUS status) {
+        MessageInfo messageInfo = new MessageInfo();
+        messageInfo.setStatus(status);
+        return messageInfo;
+    }
+
+    public static MessageInfo build(TryAccessRequest message) {
+        return build(STATUS.TRYACCESS_SENT);
+    }
+
+    public static MessageInfo build(StartAccessRequest message) {
+        MessageInfo messageInfo = build(STATUS.STARTACCESS_SENT);
+        messageInfo.setSessionId(message.getSession_id());
+        return messageInfo;
+    }
+
+    public static MessageInfo build(EndAccessRequest message) {
+        MessageInfo messageInfo = build(STATUS.ENDACCESS_SENT);
+        messageInfo.setSessionId(message.getSession_id());
+        return messageInfo;
+    }
+
+
+    public void merge(EvaluatedMessageContent message, STATUS status, STATUS positiveStatus, STATUS negativeStatus) {
+        if (this.getStatus() != status) {
+            throw new IllegalArgumentException("Wrong flow of messages!! \n status is: " + this.getStatus());
+        }
+        String evaluation = message.getEvaluation();
+        if (evaluation.equalsIgnoreCase("Permit")) {
+            this.setStatus(positiveStatus);
+        } else {
+            this.setStatus(negativeStatus);
+        }
+    }
+
+    public void merge(TryAccessResponse message) {
+        merge(message, STATUS.TRYACCESS_SENT, STATUS.TRYACCESS_PERMIT, STATUS.TRYACCESS_DENY);
+        this.setSessionId(message.getSession_id());
+    }
+
+    public void merge(StartAccessResponse message) {
+        merge(message, STATUS.STARTACCESS_SENT, STATUS.STARTACCESS_PERMIT, STATUS.STARTACCESS_DENY);
+    }
+
+    public void merge(EndAccessResponse message) {
+        merge(message, STATUS.ENDACCESS_SENT, STATUS.ENDACCESS_PERMIT, STATUS.ENDACCESS_DENY);
+    }
+
+
+    public STATUS getStatus() {
+        return status;
+    }
+
+    public void setStatus(STATUS status) {
+        this.status = status;
+    }
+
+    public String getSessionId() {
+        return sessionId;
+    }
+
+    public void setSessionId(String sessionId) {
+        this.sessionId = sessionId;
+    }
+}
