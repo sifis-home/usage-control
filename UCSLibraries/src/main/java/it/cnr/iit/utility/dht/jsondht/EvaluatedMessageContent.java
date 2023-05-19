9d8b82f75a8dd0b187d7488540bb7fc6fb079b33
marco-rasori <marco.rasori@gmail.com>
2023-05-19 17:17:45 +0200

    Add interface for classes implementing an evaluation

---
+++ UCSLibraries/src/main/java/it/cnr/iit/utility/dht/jsondht/EvaluatedMessageContent.java
@@ -0,0 +1,9 @@
+package it.cnr.iit.utility.dht.jsondht;
+
+public interface EvaluatedMessageContent {
+    String evaluation= null;
+
+    String getEvaluation();
+
+    void setEvaluation(String evaluation);
+}
