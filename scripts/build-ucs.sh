394198788900c4be591f9ba4a5ae79acf00e2818
marco-rasori <marco.rasori@gmail.com>
2023-07-17 14:48:09 +0200

    Add code coverage reports

--- scripts/build-ucs.sh
+++ scripts/build-ucs.sh
@@ -3,10 +3,10 @@
 # Build ucs
 
 # Run mvn on ucs
-# https://stackoverflow.com/questions/65092032/maven-build-failed-but-exit-code-is-still-0
+
 echo "*** Building and packaging UCS modules ***"
-# mvn clean org.jacoco:jacoco-maven-plugin:0.8.6:prepare-agent install org.jacoco:jacoco-maven-plugin:0.8.6:report | tee mvn_res
-mvn clean package -DskipTests=true | tee mvn_res
+
+mvn clean verify | tee mvn_res
 
 grep "BUILD SUCCESS" mvn_res
 EXIT_CODE=$?
